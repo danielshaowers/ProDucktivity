ffd17c17d2637b06c43bcde09f5e931ace9ac59e
Daniel Shao <50784866+danielshaowers@users.noreply.github.com>
2020-03-26 11:21:01 -0400

    added block

--- app/src/main/java/com/example/producktivity/ui/blocking/BlockActivity.java
+++ app/src/main/java/com/example/producktivity/ui/blocking/BlockActivity.java
@@ -5,40 +5,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.producktivity.R;
 
-public class BlockFragment extends Fragment {
+public class BlockActivity extends AppCompatActivity {
 
     private BlockViewModel blockViewModel;
+    private int duckClicks = 0;
 
     private final int PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS = 1;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.block_apps);
 
         blockViewModel = ViewModelProviders.of(this).get(BlockViewModel.class);
         System.out.println("creating a blocker");
-        Blocker blocker = new Blocker(getContext());
+        Blocker blocker = new Blocker(this);
         blocker.detect();
-        View root = inflater.inflate(R.layout.block_apps, container, false);
-        final TextView textView = root.findViewById(R.id.title_block);
-        blockViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        final TextView textView = findViewById(R.id.title_block);
+        Button return = findViewById(R.id.)
     }
 
 }
