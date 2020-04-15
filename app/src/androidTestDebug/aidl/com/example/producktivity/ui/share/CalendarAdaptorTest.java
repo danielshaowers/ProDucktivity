7cc1eeb6e4bc54297e90ac85ad00b60c930cb0e1
maryamiqba <55059776+maryamiqba@users.noreply.github.com>
2020-04-14 23:22:22 -0400

    Update CalendarAdaptorTest.java
    Redundant test

--- app/src/androidTestDebug/aidl/com/example/producktivity/ui/share/CalendarAdaptorTest.java
+++ app/src/androidTestDebug/aidl/com/example/producktivity/ui/share/CalendarAdaptorTest.java
@@ -10,34 +10,5 @@ import java.util.Calendar;
 import static org.junit.Assert.*;
 
 public class CalendarAdaptorTest {
-    Context context;
-    Calendar calendar;
-
-    @Test
-    public void getCount() {
-        CalendarAdaptor test = new CalendarAdaptor(context, calendar);
-        int days = 31;
-        days = CalendarAdaptor.getCount();
-    }
-
-    @Test
-    public void getItem() {
-        int item = 20;
-        CalendarAdaptor test = new CalendarAdaptor(context, calendar);
-
-        item = (int) test.getItem(20);
-    }
-
-    @Test
-    public void getItemId() {
-    }
-
-    @Test
-    public void getView() {
-    }
-
-    @Test
-    public void setSelected() {
-    }
 
 }
