ba7b28a8d8f221c9e03fc42d16e955c8f9cb7498
Daniel Shao <50784866+danielshaowers@users.noreply.github.com>
2020-04-15 01:37:10 -0400

    changed graph to be visible, scrollable

--- app/src/main/java/com/example/producktivity/ui/usage_data/UsageDataFragment.java
+++ app/src/main/java/com/example/producktivity/ui/usage_data/UsageDataFragment.java
@@ -167,11 +167,15 @@ public class UsageDataFragment extends Fragment {
 
 
         barSeries = new BarGraphSeries<com.jjoe64.graphview.series.DataPoint>(dps);
-        barSeries.setDataWidth(1);
+        barSeries.setDataWidth(3);
         graph.addSeries(barSeries);
 
         graph.getGridLabelRenderer().setHumanRounding(false, true);
         graph.getGridLabelRenderer().setNumHorizontalLabels(dps.length);
+        graph.getViewport().setScrollable(true); // enables horizontal scrolling
+        graph.getViewport().setScrollableY(true); // enables vertical scrolling
+        graph.getViewport().setScalable(true); // enables horizontal zooming and scrolling
+        graph.getViewport().setScalableY(true); // enables vertical zooming and scrolling
         //Change graph format to show the name of the app
         graph.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
             @Override
