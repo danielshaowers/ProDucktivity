2f64fc568fb1ef127e1c1adae518ececf4e033f9
Daniel Shao <50784866+danielshaowers@users.noreply.github.com>
2020-04-18 21:31:33 -0400

    category now visible for each app

--- app/src/main/java/com/example/producktivity/dbs/blacklist/Category.java
+++ app/src/main/java/com/example/producktivity/dbs/blacklist/Category.java
@@ -4,5 +4,18 @@ public enum Category {
     ALL, ART_AND_DESIGN, AUGMENTED_REALITY, AUTO_AND_VEHICLES, BEAUTY, BOOKS_AND_REFERENCE, BUSINESS,
     COMICS, COMMUNICATION, DATING, EDUCATION, ENTERTAINMENT, EVENTS, FAMILY, FINANCE, FOOD_AND_DRINK,
     GAME, GOOGLE_CAST, HEALTH_AND_FITNESS, HOUSE_AND_HOME, LIBRARIES_AND_DEMO, LIFESTYLE, PRODUCKTIVITY,
-    MAPS_AND_NAVIGATION, MEDICAL, SYSTEM, TRAVEL_AND_LOCAL, VIDEO_PLAYERS
+    MAPS_AND_NAVIGATION, MEDICAL, SYSTEM, TRAVEL_AND_LOCAL, VIDEO_PLAYERS;
+
+    @Override
+    public String toString(){
+        String name = name();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < name.length(); i++){
+            if (name.charAt(i) == '_')
+                sb.append(' ');
+            else
+                sb.append(name.charAt(i));
+        }
+        return sb.toString();
+    }
 }
--- app/src/main/java/com/example/producktivity/ui/blockSelect/SelectAdapter.java
+++ app/src/main/java/com/example/producktivity/ui/blockSelect/SelectAdapter.java
@@ -32,6 +32,7 @@ public class SelectAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             private final Button appSelect;
             private final CardView card;
             private final Button save;
+            private final TextView cat;
 
             public selectViewHolder(View itemView) {
                 super(itemView);
@@ -43,6 +44,7 @@ public class SelectAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                 weekLimit = itemView.findViewById(R.id.weekly_limit);
                 dayLimit = itemView.findViewById(R.id.daily_limit);
                 card = itemView.findViewById(R.id.limit_card);
+                cat = itemView.findViewById(R.id.show_cat);
             }
         }
 
@@ -90,6 +92,7 @@ public class SelectAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                 holder.appSelect.setText(current.getAppName());
                 holder.weekLimit.setText(BlacklistEntry.longToString(current.getWeekLimit()));
                 holder.dayLimit.setText(BlacklistEntry.longToString(current.getDayLimit()));
+                holder.cat.setText(current.getCategory().toString());
                 holder.save.setOnClickListener(v -> { //when done is clicked, hide and save the values
                     holder.card.setVisibility(View.INVISIBLE);
                     holder.appSelect.setVisibility(View.VISIBLE);
